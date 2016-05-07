77030c7f4a51bb16c91bce1334dede21a843b1d7
Nathan Radmall <nathanradmall@gmail.com>
2016-05-07 14:14:50 -0600

    server proxy stuff

--- src/proxy/ClientCommunicator.java
+++ src/proxy/ClientCommunicator.java
@@ -1,5 +1,19 @@
 package proxy;
 
+import poller.InvalidClientCommunicatorException;
+
 public class ClientCommunicator {
 
+	/**
+	 * description: takes an api command from the proxy or poller and sends it to the server
+	 * @pre input has to be put into an input object depending on the function
+	 * @post return an object that will be passed back to proxy and turned into function output object
+	 */
+	 public void send(){
+	 //public void send(inputObject){
+		 // this will take in an input object but they haven't been created yet
+		 
+	 }
+	 
+	
 }
---
+++ src/proxy/RealProxy.java
@@ -0,0 +1,202 @@
+package proxy;
+
+import java.util.ArrayList;
+
+import javax.annotation.Resource;
+
+import model.Game;
+import model.Person;
+import shared.locations.EdgeLocation;
+import shared.locations.HexLocation;
+import shared.locations.VertexLocation;
+
+public class RealProxy implements IServer{
+
+	@Override
+	public void userLogin(String username, String password) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void userRegister(String username, String password) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void gamesList() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void gamesCreate(String name, ArrayList randomTiles, ArrayList randomNumbers, ArrayList randomPorts) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void gameJoin(Person user, Integer gameID, String color) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void gamesSave(Integer gameID, String fileName) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void gamesLoad(String fileName) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void gameModel(Person user, Integer versionNumber) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void gameReset(Person user, Game game) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void gameCommandsGet(Person user, Game game) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void gameCommandsPost(Person user, Game game) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void gameListAI() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void gameAddAI(Person user, Game game, String AIType) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void utilChangeLogLevel(String loggingLevel) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void sendChat(String content) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void acceptTrade(Boolean willAccept) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void discardCards(ArrayList discardedCards) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void rollNumber(Integer number) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void buildRoad(Boolean free, EdgeLocation roadLocation) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void buildSettlement(Boolean free, VertexLocation vertexLocation) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void buildCity(VertexLocation vertexLocation) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void offerTrade(ArrayList of_er, Integer playerIndex) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void maritimeTrade(Integer ratio, Resource inputResource, Resource outputResource) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void robPlayer(HexLocation location, Integer playerIndex) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void finishTurn() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void buyDevCard() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void Soldier(HexLocation location, Integer playerIndex) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void Year_of_Plenty(Resource resource1, Resource resource2) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void Road_Building(EdgeLocation spot1, EdgeLocation spot2) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void Monopoly(Resource resource) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void Monument() {
+		// TODO Auto-generated method stub
+		
+	}
+	
+
+}
