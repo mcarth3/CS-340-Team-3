acc03e7e84df37fa6fc1076a40e044ab357ac70c
Nathan Radmall <nathanradmall@gmail.com>
2016-05-09 16:43:16 -0600

    added demo back int, ant working, tests

--- src/poller/ServerPoller.java
+++ src/poller/ServerPoller.java
@@ -4,7 +4,7 @@ import java.util.Timer;
 import java.util.TimerTask;
 import client.base.Controller;
 import model.Game;
-import poller.modeljsonparser.ModelParser;
+import poller.modeljsonparser.modelparser;
 import proxy.MockProxy;
 
 
