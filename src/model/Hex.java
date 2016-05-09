73399544c591f9b39dda7acabdefe96acb4ee5aa
Jesse <mcarth3@gmail.com>
2016-05-08 23:20:31 -0600

    changing back

--- src/model/Hex.java
+++ src/model/Hex.java
@@ -3,7 +3,7 @@ package model;
 import shared.definitions.HexType;
 import shared.locations.HexLocation;
 /**
- * The hex cl represents a hex on the map. It has a location, resource type, 
+ * The hex class represents a hex on the map. It has a location, resource type, 
  *	and an int.
  * @author Jesse McArthur
  *	
