90cc263442d291a9930aa283f42b67fa204fa131
Jesse Robinson <jesser1212@gmail.com>
2016-05-10 15:57:41 -0600

    Added Facade (for Controller interaction with model) with multiple Can-Do's. Also added jUnit package so all can create jUnit tests on classes.

---
+++ src/model/Facade.java
@@ -0,0 +1,47 @@
+package model;
+
+import shared.definitions.DevCardType;
+import shared.definitions.ResourceType;
+
+/**
+ * A Facade for the Controller to interact with the Game Model. This will also contain several different can-Do's for the
+ * game and players (e.g. canPlayDevCard)
+ * Created by Jesse on 5/10/2016.
+ */
+public class Facade {
+    private Game theGame;
+
+    public Facade()
+    {
+
+    }
+
+    public void canPlayerBuySettlement(String username)
+    {
+
+    }
+
+    public void canPlayerBuyCity(String username)
+    {
+
+    }
+
+    public void canPlayerBuyRoad(String username)
+    {
+
+    }
+
+    public void canPlayerAcceptTrade(String username, ResourceType type, int resourceNumber)
+    {
+
+    }
+
+    public void canPlayerPlayDevCard(String username, DevCardType type)
+    {
+
+    }
+
+
+
+
+}
