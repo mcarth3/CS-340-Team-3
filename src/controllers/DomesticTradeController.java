d0b3f62294d40e4dad6c4b90c887c1196aba73e0
Jesse Robinson <jesser1212@gmail.com>
2016-05-12 23:16:36 -0600

    modified Controllers for Roll, Bank, DomesticTrade, ResourceBar_DevCard, and MaritimeTrade to have blank functions that help describe what I think they should be doing

--- src/controllers/DomesticTradeController.java
+++ src/controllers/DomesticTradeController.java
@@ -1,7 +1,16 @@
 package controllers;
 
+import shared.definitions.ResourceType;
+
 /**
  * Created by Jesse on 5/12/2016.
  */
 public class DomesticTradeController {
+    public DomesticTradeController() {
+    }
+
+    public boolean tradePersonForResource(String username, ResourceType type, int amount)
+    {
+        return false;
+    }
 }
--- src/controllers/RollController.java
+++ src/controllers/RollController.java
@@ -4,4 +4,12 @@ package controllers;
  * Created by Jesse on 5/12/2016.
  */
 public class RollController {
+
+    public RollController() {
+    }
+
+    public void personRolled(String username)
+    {
+
+    }
 }
