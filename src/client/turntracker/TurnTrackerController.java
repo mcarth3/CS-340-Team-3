8a9b1fa267e6e949a8497ddb2d261c4a1dcb741f
Mike Towne <miketowne@gmail.com>
2016-05-19 00:09:02 -0600

    turntracker not crashing, still needs updated states

--- src/client/turntracker/TurnTrackerController.java
+++ src/client/turntracker/TurnTrackerController.java
@@ -54,74 +54,74 @@ public class TurnTrackerController extends Controller implements ITurnTrackerCon
 		//if (getView() != null){
 			getView().setLocalPlayerColor(CatanColor.RED);
 	//	}
-//		GameManager gm = GameManager.getSingleton();
+		GameManager gm = GameManager.getSingleton();
 		
-//		getView().setLocalPlayerColor(gm.colorTemp);
-//		
-//		TurnTracker turnTracker = manager.getModel().getTurnTracker();
-//		ArrayList<Player> players = manager.getModel().getPlayers();
-//		for(int i = 0; i < players.size(); i++) {
-//			boolean largestArmy = false;
-//			boolean longestRoad = false;
-//			boolean highLight = false;
-//			int playerIndex = -2;
-//			Player currentplayer = players.get(i);
-//			if(currentplayer != null) {
-//				playerIndex = currentplayer.getPlayerIndex();
-//				System.out.println(getView().toString());
-//				if ((currentplayer !=null)){
-//				//	getView().initializePlayer(playerIndex, currentplayer.getName(), currentplayer.getColor());
-//				}
-//				if(turnTracker.getCurrentPlayer() == playerIndex) {
-//					highLight = true;
-//				}
-//				if(turnTracker.getLargestArmy() == playerIndex) {
-//					largestArmy = true;
-//				}
-//				if(turnTracker.getLongestRoad() == playerIndex) {
-//					longestRoad = true;
-//				}
-//				getView().updatePlayer(playerIndex, currentplayer.getVictoryPoints(), highLight, largestArmy, longestRoad);	
-//			}
-//		}
+		getView().setLocalPlayerColor(gm.colorTemp);
+		
+		TurnTracker turnTracker = manager.getModel().getTurnTracker();
+		ArrayList<Player> players = manager.getModel().getPlayers();
+		for(int i = 0; i < players.size(); i++) {
+			boolean largestArmy = false;
+			boolean longestRoad = false;
+			boolean highLight = false;
+			int playerIndex = -2;
+			Player currentplayer = players.get(i);
+			if(currentplayer != null) {
+				playerIndex = currentplayer.getPlayerIndex();
+		//		System.out.println(getView().toString());
+				if ((currentplayer !=null)){
+					getView().initializePlayer(playerIndex, currentplayer.getName(), currentplayer.getColor());
+				}
+				if(turnTracker.getCurrentPlayer() == playerIndex) {
+					highLight = true;
+				}
+				if(turnTracker.getLargestArmy() == playerIndex) {
+					largestArmy = true;
+				}
+				if(turnTracker.getLongestRoad() == playerIndex) {
+					longestRoad = true;
+				}
+				getView().updatePlayer(playerIndex, currentplayer.getVictoryPoints(), highLight, largestArmy, longestRoad);	
+			}
+		}
 
 	}
 	public void update(){
 		initFromModel();
-//		Game model = manager.getModel();
-//		boolean enableButton = false;
-//		String message = "Waiting for Other Players";
-//		if(model.getTurnTracker().getCurrentPlayer() == manager.getthisplayer().getPlayerIndex()) {
-//			switch(model.getTurnTracker().getStatus()) {
-//			case "Discarding":
-//				message = "Discarding";
-//				break;
-//			case "FirstRound":
-//				message = "First Round";
-//				break;
-//			case "Robbing"://double check if these are the right states
-//				message = "Robbing";
-//				break;
-//			case "Rolling":
-//				message = "Rolling";
-//				break;
-//			case "SecondRound":
-//				message = "Second Round";
-//				break;
-//			case "Playing":
-//				if (model != null){
-//					if (model.canFinishTurn(manager.getthisplayer().getPlayerID())){
-//						message = "End Turn";
-//						enableButton = true;
-//						break;
-//					}
-//				}
-//			default:
-//				message = "Waiting for Other Players";
-//				break;
-//			}	
-//		}
-//		this.getView().updateGameState(message, enableButton);
+		Game model = manager.getModel();
+		boolean enableButton = false;
+		String message = "Waiting for Other Players";
+		if(model.getTurnTracker().getCurrentPlayer() == manager.getthisplayer().getPlayerIndex()) {
+			switch(model.getTurnTracker().getStatus()) {
+			case "Discarding":
+				message = "Discarding";
+				break;
+			case "FirstRound":
+				message = "First Round";
+				break;
+			case "Robbing":
+				message = "Robbing";
+				break;
+			case "Rolling":
+				message = "Rolling";
+				break;
+			case "SecondRound":
+				message = "Second Round";
+				break;
+			case "Playing":
+				if (model != null){
+					if (model.canFinishTurn(manager.getthisplayer().getPlayerID())){
+						message = "End Turn";
+						enableButton = true;
+						break;
+					}
+				}
+			default:
+				message = "Waiting for Other Players";
+				break;
+			}	
+		}
+		this.getView().updateGameState(message, enableButton);
 	}
 
 }
