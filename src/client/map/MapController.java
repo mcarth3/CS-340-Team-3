d51afd0b5d2ea43d69b3a7f9bcc84c46a906ef5a
Mike Towne <miketowne@gmail.com>
2016-05-23 21:47:06 -0600

    removed unused method (init from model) form map controller

--- src/client/map/MapController.java
+++ src/client/map/MapController.java
@@ -40,7 +40,6 @@ public class MapController extends Controller implements IMapController {
 
 		setRobView(robView);
 
-		initFromModel();
 	}
 
 	@Override
@@ -55,83 +54,6 @@ public class MapController extends Controller implements IMapController {
 
 	private void setRobView(IRobView robView) {
 		this.robView = robView;
-	}
-
-	protected void initFromModel() {
-
-		// <temp>
-
-		// Random rand = new Random();
-		//
-		// for (int x = 0; x <= 3; ++x) {
-		//
-		// int maxY = 3 - x;
-		// for (int y = -3; y <= maxY; ++y) {
-		// int r = rand.nextInt(HexType.values().length);
-		// HexType hexType = HexType.values()[r];
-		// HexLocation hexLoc = new HexLocation(x, y);
-		// getView().addHex(hexLoc, hexType);
-		// getView().placeRoad(new EdgeLocation(hexLoc, EdgeDirection.N),
-		// CatanColor.RED);
-		// getView().placeRoad(new EdgeLocation(hexLoc, EdgeDirection.SW),
-		// CatanColor.BLUE);
-		// getView().placeRoad(new EdgeLocation(hexLoc, EdgeDirection.S),
-		// CatanColor.ORANGE);
-		// getView().placeSettlement(new VertexLocation(hexLoc,
-		// VertexDirection.NorthWest), CatanColor.GREEN);
-		// getView().placeCity(new VertexLocation(hexLoc,
-		// VertexDirection.NorthEast), CatanColor.PURPLE);
-		// }
-		//
-		// if (x != 0) {
-		// int minY = x - 3;
-		// for (int y = minY; y <= 3; ++y) {
-		// int r = rand.nextInt(HexType.values().length);
-		// HexType hexType = HexType.values()[r];
-		// HexLocation hexLoc = new HexLocation(-x, y);
-		// getView().addHex(hexLoc, hexType);
-		// getView().placeRoad(new EdgeLocation(hexLoc, EdgeDirection.N),
-		// CatanColor.RED);
-		// getView().placeRoad(new EdgeLocation(hexLoc, EdgeDirection.SW),
-		// CatanColor.BLUE);
-		// getView().placeRoad(new EdgeLocation(hexLoc, EdgeDirection.S),
-		// CatanColor.ORANGE);
-		// getView().placeSettlement(new VertexLocation(hexLoc,
-		// VertexDirection.NorthWest), CatanColor.GREEN);
-		// getView().placeCity(new VertexLocation(hexLoc,
-		// VertexDirection.NorthEast), CatanColor.PURPLE);
-		// }
-		// }
-		// }
-		//
-		// PortType portType = PortType.BRICK;
-		// getView().addPort(new EdgeLocation(new HexLocation(0, 3),
-		// EdgeDirection.N), portType);
-		// getView().addPort(new EdgeLocation(new HexLocation(0, -3),
-		// EdgeDirection.S), portType);
-		// getView().addPort(new EdgeLocation(new HexLocation(-3, 3),
-		// EdgeDirection.NE), portType);
-		// getView().addPort(new EdgeLocation(new HexLocation(-3, 0),
-		// EdgeDirection.SE), portType);
-		// getView().addPort(new EdgeLocation(new HexLocation(3, -3),
-		// EdgeDirection.SW), portType);
-		// getView().addPort(new EdgeLocation(new HexLocation(3, 0),
-		// EdgeDirection.N), portType);
-		//
-		// getView().placeRobber(new HexLocation(0, 0));
-		//
-		// getView().addNumber(new HexLocation(-2, 0), 2);
-		// getView().addNumber(new HexLocation(-2, 1), 3);
-		// getView().addNumber(new HexLocation(-2, 2), 4);
-		// getView().addNumber(new HexLocation(-1, 0), 5);
-		// getView().addNumber(new HexLocation(-1, 1), 6);
-		// getView().addNumber(new HexLocation(1, -1), 8);
-		// getView().addNumber(new HexLocation(1, 0), 9);
-		// getView().addNumber(new HexLocation(2, -2), 10);
-		// getView().addNumber(new HexLocation(2, -1), 11);
-		// getView().addNumber(new HexLocation(2, 0), 12);
-
-		// </temp>
 	}
 
 	public void setUp() {
