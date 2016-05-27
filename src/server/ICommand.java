6b1582e76f36170eb9fc715a2a300f16f26824ab
Mike Towne <miketowne@gmail.com>
2016-05-27 07:18:27 -0600

    added my first command, and the first json object (unfinished)

--- src/server/ICommand.java
+++ src/server/ICommand.java
@@ -1,17 +1,20 @@
 package server;
 
 /**
- * Created by Jesse on 5/26/2016.
- * interface for all Commands.
+ * Created by Jesse on 5/26/2016. interface for all Commands.
  */
 public interface ICommand {
 
-    /**
-     * called by a Handler to execute whatever the Command needs.
-     * (e.g. if a RoadBuilding command came in from the server, one would create
-     * a RoadBuildingCommand and call execute() to cause the ServerModel to change)
-     * @pre: request from HTTP decoded and ServerModel initialized
-     * @post: server model has been changed by a controller.
-     */
-    void execute();
+	/**
+	 * called by a Handler to execute whatever the Command needs. (e.g. if a
+	 * RoadBuilding command came in from the server, one would create a
+	 * RoadBuildingCommand and call execute() to cause the ServerModel to
+	 * change)
+	 * 
+	 * @pre: request from HTTP decoded and ServerModel initialized
+	 * @post: server model has been changed by a controller.
+	 */
+	void execute();
+
+	void execute(Object data);
 }
