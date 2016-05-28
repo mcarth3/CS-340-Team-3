90eb1347d48b2f5b8b2a03ea987bbc332ef294e1
Nathan Radmall <nathanradmall@gmail.com>
2016-05-27 18:52:45 -0600

    server works now

--- src/server/Server.java
+++ src/server/Server.java
@@ -1,51 +1,62 @@
 package server;
 
+import java.io.*;
+import java.net.*;
+import java.rmi.ServerException;
+import java.util.logging.*;
+import com.sun.net.httpserver.*;
+//import server.handlers.*;
 import java.io.IOException;
 import java.net.InetSocketAddress;
 
-import com.sun.net.httpserver.HttpServer;
 
 public class Server {
+	
 	private static int portNum = 8081;
 	private static final int MAX_WAITING_CONNECTIONS = 10;
-
-	public static void main(String[] args) {
-		Server server = new Server(portNum);
-		server.run();
+	private HttpServer server;
+	
+	private Server() {
+		return;
 	}
-
-	private void run() {
+	
+	private void run(int port) {
+		
 		try {
-			Thread.sleep(1000);
-		} catch (InterruptedException ex) {
-		}
-
-		HttpServer http_server;
-		try {
-			http_server = HttpServer.create(new InetSocketAddress(portNum), MAX_WAITING_CONNECTIONS);
-		} catch (IOException e) {
-			e.printStackTrace();
+			server = HttpServer.create(new InetSocketAddress(port), MAX_WAITING_CONNECTIONS);
+		} 
+		catch (IOException e) {			
 			return;
 		}
-		http_server.setExecutor(null);
-		// http_server.createContext("/validate_user", validate_user_handler);
-		// //worked when handlers were in this file
-		// http_server.createContext("/get_projects", get_project_handler);
-		// http_server.createContext("/get_sample_image", get_sample_image_handler);
-		// http_server.createContext("/download_batch", download_batch_handler);
-		// http_server.createContext("/submit_batch", submit_batch_handler);
-		// http_server.createContext("/get_fields", get_fields_handler);
-		// http_server.createContext("/search", search_handler);
-		// http_server.createContext("/", download_file_handler);
-		http_server.start();
-	}
 
-	private Server(int portNum) {
-		Server.portNum = portNum;
+		server.setExecutor(null); // use the default executor
+		server.createContext("/user/login", userLogin);
+		server.createContext("/user/register", userRegister);
+		server.createContext("/games/list", gamesList);
+		server.createContext("/games/create", gamesCreate);
+		server.createContext("/games/join", gamesJoin);
+		server.createContext("/game/model", gameModel);
+		server.createContext("/moves/sendChat", movesSendChat);
+		
+		System.out.println("Server is starting"); 
+
+		server.start();
 	}
+	public Handlers handlers = new Handlers(); 
+	private HttpHandler userLogin =  handlers.getUserLoginHandler();
+	private HttpHandler userRegister =  handlers.getUserRegisterHandler();
+	private HttpHandler gamesList =  handlers.getGamesListHandler();
+	private HttpHandler gamesCreate =  handlers.getGamesCreateHandler();
+	private HttpHandler gamesJoin =  handlers.getGamesJoinHandler();
+	private HttpHandler gameModel =  handlers.getGameModelHandler();
+	private HttpHandler movesSendChat =  handlers.getMovesSendChatHandler();
 
 	public static void decode(String[] args) {
 
+	}
+	
+	public static void main(String[] args) {
+		new Server().run(portNum);
 	}
 
 }
