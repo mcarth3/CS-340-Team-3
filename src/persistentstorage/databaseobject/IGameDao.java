7dac95d2527bb7dd82966059dad02891b75fa1d4
Jesse Robinson <jesser1212@gmail.com>
2016-06-13 00:25:27 -0600

    added files for FlatFileGameDAO, RDBGameDAO, and IGameDAO

--- src/persistentstorage/databaseobject/IGameDao.java
+++ src/persistentstorage/databaseobject/IGameDao.java
@@ -1,5 +1,58 @@
 package persistentstorage.databaseobject;
 
-public interface IGameDao {
+import model.Game;
+import server.CommandList;
+import shared.definitions.CatanColor;
+
+import java.util.ArrayList;
+
+/**
+ * interface implemented by FlatFileGameDAO and RDBGameDAO
+ * includes functions getCommands, clearCommands, storeGame,  getGameByID, getListOfGames
+ */
+public interface IGameDAO {
+
+    /**
+     *
+     * @param gameID
+     * @pre gameID is valid
+     * @post the CommandList of the specified game is returned
+     */
+    public CommandList getCommands(Integer gameID);
+
+    /**
+     *
+     * @param gameID
+     * @pre gameID is valid
+     * @post the CommandList of the specified game is cleared
+     */
+    public void clearCommands(Integer gameID);
+
+    /**
+     *
+     * @param gameID
+     * @param theGame
+     * @pre gameID is valid, theGame is valid Game object
+     * @post the specified Game is saved in the specified form of file (RDB or FlatFile, depending on the object implementing this interface)
+     */
+    public void storeGame(Integer gameID, Game theGame);
+
+    /**
+     *
+     * @param gameID
+     * @pre gameID is valid
+     * @post the specified Game is returned to the caller.
+     */
+    public Game getGameByID(Integer gameID);
+
+    /**
+     *
+     * @pre a list of Games has already been established here
+     * @post the list of all saved Games is returned to the caller.
+     */
+    public ArrayList<Game> getListOfGames(Integer gameID);
+
+
+
 
 }
---
+++ src/persistentstorage/databaseobject/RDBGameDAO.java
@@ -0,0 +1,36 @@
+package persistentstorage.databaseobject;
+
+import model.Game;
+import server.CommandList;
+
+import java.util.ArrayList;
+
+/**
+ * Created by Jesse on 6/12/2016.
+ */
+public class RDBGameDAO implements IGameDAO {
+    @Override
+    public CommandList getCommands(Integer gameID) {
+        return null;
+    }
+
+    @Override
+    public void clearCommands(Integer gameID) {
+
+    }
+
+    @Override
+    public void storeGame(Integer gameID, Game theGame) {
+
+    }
+
+    @Override
+    public Game getGameByID(Integer gameID) {
+        return null;
+    }
+
+    @Override
+    public ArrayList<Game> getListOfGames(Integer gameID) {
+        return null;
+    }
+}
